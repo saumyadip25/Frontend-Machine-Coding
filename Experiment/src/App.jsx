b6cd2bbfc4f85464453fafe97726f4456ba0d939
Your Name <your-email@example.com>
2025-08-10 17:55:14 +0530

    initial commit

---
+++ Experiment/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useRef } from "react";
+
+const getTime = (counter) => {
+  const HH = Math.floor(counter / 3600);
+  const hh = `${HH}`.padStart(2, "0");
+  const MM = Math.floor((counter % 3600) / 60);
+  const mm = `${MM}`.padStart(2, "0");
+  const SS = `${counter % 60}`.padStart(2, "0");
+
+  return hh + ":" + mm + ":" + SS;
+};
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalId = useRef(null);
+
+  const handleStart = () => {
+    setIsRunning(true);
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+    intervalId.current = setInterval(() => {
+      setCounter((count) => count + 1);
+    }, 1000);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <h1>StopWatch</h1>
+      <div>Time in HH:MM:SS format</div>
+      <div>{getTime(counter)}</div>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
